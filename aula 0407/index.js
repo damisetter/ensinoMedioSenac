b1ee7563a6412470ae86b7c8f02e34d22fe81b52
damisetter <damarisetterpereira3@gmail.com>
2024-08-16 11:44:49 -0300

    Add files via upload

---
+++ aula 0407/index.js
@@ -0,0 +1,9 @@
+const seriesBoas = ['breaking bad', 'brooklyn nine-nine']
+seriesBoas.includes("breaking bad")
+seriesBoas.includes("game of thrones")
+
+let lista = ['batata', 'cebola', 'tomate']
+
+lista.push('mamão', 'sabão em pó')
+
+console.log(lista)
