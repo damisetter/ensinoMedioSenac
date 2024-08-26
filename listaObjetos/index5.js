0f157be53b79b35f69a88dee7260e8f20c8f1d90
damisetter <damarisetterpereira3@gmail.com>
2024-08-26 12:29:17 -0300

    Update index5.js

--- listaObjetos/index5.js
+++ listaObjetos/index5.js
@@ -1,25 +1,40 @@
 
-let carrinho = [];
-
+let carrinho = []
 let fruta1 = {
-    nome: 'Maçã',
+    nome: "Maçã",
     disponibilidade: true
 }
-
 let fruta2 = {
-    nome: 'Banana',
+    nome: "Banana",
     disponibilidade: true
 }
-
 let fruta3 = {
-    nome: 'Laranja',
+    nome: "Laranja",
     disponibilidade: true
 }
-
 carrinho.push(fruta1)
 carrinho.push(fruta2)
 carrinho.push(fruta3)
 
+
+function listarItens(carrinho) {
+    carrinho.forEach(fruta => {
+        console.log(`Nome: ${fruta.nome}, Disponibilidade: ${fruta.disponibilidade}`)
+    })
+}
+
+
+function alterarDisponibilidade(fruta) {
+    fruta.disponibilidade = !fruta.disponibilidade
+    return fruta
+}
+
+console.log("Itens no carrinho:")
+listarItens(carrinho)
+
+alterarDisponibilidade(fruta1)
+console.log("\nItens no carrinho após alterar disponibilidade da maçã:")
+listarItens(carrinho)
 
 
  
